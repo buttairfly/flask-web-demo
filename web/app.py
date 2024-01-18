14825a8747a3415824826e00c3f8d2c9bd74d0aa
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2024-01-18 02:39:21 +0100

    Initial commit

---
+++ web/app.py
@@ -0,0 +1,19 @@
+import webbrowser
+from flask import Flask, render_template, request
+from connexion import FlaskApp
+import os, signal
+
+app = FlaskApp(__name__)
+app.add_api("swagger.yml")
+
+@app.route("/")
+def index():
+    return render_template('index.html.j2')
+
+def exit():
+    os.kill(os.getpid(),signal.SIGINT)
+    return ("Shutdown")
+
+if __name__ == "__main__":
+    webbrowser.open_new_tab('http://127.0.0.1:3000/')
+    app.run(port=3000)
