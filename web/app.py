35ac4f718c97446aac8f9b2925b8675176dcfea8
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2024-01-19 01:11:20 +0100

    Add content

--- web/app.py
+++ web/app.py
@@ -1,7 +1,9 @@
+import threading
 import webbrowser
 from flask import Flask, render_template, request
 from connexion import FlaskApp
 import os, signal
+from measurements import read_all
 
 app = FlaskApp(__name__)
 app.add_api("swagger.yml")
@@ -9,6 +11,10 @@ app.add_api("swagger.yml")
 @app.route("/")
 def index():
     return render_template('index.html.j2')
+
+@app.route("/measurements")
+def measurements():
+    return render_template('index.html.j2', measurements=read_all())
 
 def exit():
     os.kill(os.getpid(),signal.SIGINT)
@@ -20,9 +26,18 @@ def ui():
 def openapi():
     pass
 
-def main():
+
+def open_browser():
     webbrowser.open_new_tab('http://127.0.0.1:3000')
+
+def main():
+    # delay browser opening
+    t = threading.Timer(1, open_browser)
+    t.start()
+    # start webapp
     app.run(port=3000)
+    # cancel browser starting when aborted...
+    t.cancel()
 
 if __name__ == "__main__":
     main()
--- web/measurements.py
+++ web/measurements.py
@@ -1,25 +1,25 @@
 from datetime import datetime
+import random, string
 
-def get_timestamp():
+def get_timestamp() -> str:
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-measurements = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
+def get_value() -> str:
+    return f"{random.random():.3f}"
+
+def randomword(length: int) -> str:
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for _ in range(length))
+
+def generate_measurement() -> dict[str,str]:
+    return dict({
+        "name": randomword(10),
+        "value": get_value(),
         "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    }
-}
+    })
+
+def generate_set(length: int) -> list[dict[str,str]]:
+    return list([generate_measurement() for _ in range(length)])
 
 def read_all():
-    return list(measurements.values())
+    return generate_set(20)
